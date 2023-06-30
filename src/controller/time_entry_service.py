a39f9ff900fd5eda67c68ab3d1ebb42d75b937ee
Sebastian Führ <s.fuehr1@gmail.com>
2023-06-30 08:39:17 +0200

    Allow time entry DB merge

--- src/controller/time_entry_service.py
+++ src/controller/time_entry_service.py
@@ -1,9 +1,17 @@
 from sqlalchemy import select
 from ..model.time_entry import TimeEntry
 
+
 class TimeEntryService():
 
     def get_all(db_session):
         stmt = select(TimeEntry)
         results = db_session.execute(stmt)
         return results.scalars()
+
+    def merge(db_session, te: TimeEntry):
+        """Inserts or updates a record.
+        """
+        output = db_session.merge(te)
+        db_session.commit()
+        print(f'merge outpu: {output.to_list()}')
