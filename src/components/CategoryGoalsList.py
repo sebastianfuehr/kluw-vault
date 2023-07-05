a00c000cff933cec85511e01c5fc7407e2b26bbf
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-05 16:50:14 +0300

    Add category list placeholder frame in notebook

---
+++ src/components/CategoryGoalsList.py
@@ -0,0 +1,17 @@
+import ttkbootstrap as tb
+
+from ..controller.project_category_goal_stats_controller import ProjectCategoryGoalStatsController
+from ..model.project_category_goal import ProjectCategoryGoal
+
+
+class CategoryGoalsList(tb.Frame):
+    def __init__(self, parent, app):
+        super().__init__(master=parent)
+        self.app = app
+        self.goals_controller = ProjectCategoryGoalStatsController(self.app.session)
+
+        test = tb.Label(self, text='Test')
+        test.pack()
+
+        res = self.goals_controller.get_active_goals()
+        print(res)
