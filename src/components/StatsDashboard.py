dc33ffe829754613d4df7625c7915b82c13c15c0
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-06 10:03:17 +0300

    Format time per day graph

--- src/components/StatsDashboard.py
+++ src/components/StatsDashboard.py
@@ -74,27 +74,39 @@ class GraphTimePerDay(tb.Frame):
         super().__init__(master=parent)
         self.app = app
         self.data = data
+
+        lbl_heading = tb.Label(
+            self,
+            text='Time per Day',
+            font=(None, 24, 'bold'),
+            anchor='center'
+        )
+        lbl_heading.pack(side='top', fill='x', pady=10)
+
         # Figure
         figure = plt.Figure()
-        figure.subplots_adjust(left=0.1, bottom=0.1, right=1, top=1)
+        figure.subplots_adjust(left=0.02, bottom=0.1, right=1, top=1)
         figure.patch.set_facecolor(self.app.style.colors.bg)
+        figure.autofmt_xdate()
 
         # Graph
         tmp_data = self.data
         data_by_date = tmp_data.groupby(pd.Grouper(key='Date', freq='D'))['Duration'].sum()
         data_by_date = data_by_date.reset_index()
-        data_by_date['Minutes'] = data_by_date['Duration'].dt.total_seconds() / 60
+        data_by_date['Minutes'] = data_by_date['Duration'].dt.total_seconds() / 60 / 60
+        data_by_date['Average'] = data_by_date['Minutes'].mean()
         axis = figure.add_subplot(111)
         line = axis.plot(data_by_date['Date'], data_by_date['Minutes'])[0]
+        line_avg = axis.plot(data_by_date['Date'], data_by_date['Average'])[0]
+        line.set_color(HIGHLIGHT_COLOR)
 
         axis.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        axis.set_ylim(ymin=0)
         axis.set_facecolor(self.app.style.colors.bg)
         for side in ['left', 'bottom']:
             axis.spines[side].set_color('white')
         for side in ['right', 'top']:
             axis.spines[side].set_color(self.app.style.colors.bg)
-        line.set_color(HIGHLIGHT_COLOR)
-        figure.autofmt_xdate()
 
         # Ticks
         axis.tick_params(axis='x', colors='white')
@@ -102,7 +114,7 @@ class GraphTimePerDay(tb.Frame):
 
         # Widget
         fig_widget = FigureCanvasTkAgg(figure, master=self)
-        fig_widget.get_tk_widget().pack()
+        fig_widget.get_tk_widget().pack(side='top', fill='both')
 
 
 class FilterPanel(tb.Frame):
