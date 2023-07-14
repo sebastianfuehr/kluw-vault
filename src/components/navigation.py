1d2ae47e6a77bd2ce05372edd7dd641d7ab1127b
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-14 13:38:33 +0300

    Implement button panel support for key-value pairs

--- src/components/navigation.py
+++ src/components/navigation.py
@@ -13,18 +13,37 @@ class ButtonPanel(tb.Frame): # pylint: disable=too-many-ancestors
         A list of strings which are to appear on the buttons.
     styling: {padx: int, pady: int, font: (str, int), colors: {text: str, highlight: str}}
     """
-    def __init__(self, parent, ttk_string_var, labels, styling):
+    def __init__(
+            self,
+            parent,
+            ttk_string_var,
+            labels,
+            styling,
+            ttk_key_var=None
+        ):
         super().__init__(master=parent)
 
         self.buttons = []
-        for tab in labels:
-            self.buttons.append(TextButton(
-                master=self,
-                text=tab,
-                tab_nav_str=ttk_string_var,
-                button_group=self.buttons,
-                styling=styling
-            ))
+        if isinstance(labels, list):
+            for tab in labels:
+                self.buttons.append(TextButton(
+                    master=self,
+                    text=tab,
+                    tab_nav_str=ttk_string_var,
+                    button_group=self.buttons,
+                    styling=styling
+                ))
+        else:
+            for key, value in labels.items():
+                self.buttons.append(TextButton(
+                    master=self,
+                    text=value,
+                    tab_nav_str=ttk_string_var,
+                    button_group=self.buttons,
+                    styling=styling,
+                    key=key,
+                    tab_nav_int=ttk_key_var
+                ))
 
 
 class TextButton(tb.Label): # pylint: disable=too-many-ancestors
@@ -36,7 +55,16 @@ class TextButton(tb.Label): # pylint: disable=too-many-ancestors
         highlighted, all other buttons of the same group are unselected
         via the unselect() function.
     """
-    def __init__(self, master, text, tab_nav_str, button_group, styling):
+    def __init__(
+            self,
+            master,
+            text,
+            tab_nav_str,
+            button_group,
+            styling,
+            key=None,
+            tab_nav_int=None
+        ):
         self.colors = styling['colors']
         super().__init__(
             master=master,
@@ -47,7 +75,9 @@ class TextButton(tb.Label): # pylint: disable=too-many-ancestors
         self.bind('<Button-1>', self.select_handler)
 
         self.button_group = button_group
+        self.key = key
         self.text = text
+        self.tab_nav_int = tab_nav_int
         self.tab_nav_str = tab_nav_str
 
         if tab_nav_str.get() == text:
@@ -63,6 +93,8 @@ class TextButton(tb.Label): # pylint: disable=too-many-ancestors
     def select_handler(self, *_args):
         """Callback method for when the label is clicked on.
         """
+        if self.tab_nav_int is not None:
+            self.tab_nav_int.set(self.key)
         self.tab_nav_str.set(self.text)
         for button in self.button_group:
             button.unselect()
