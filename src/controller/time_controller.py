ac7d405efea3d2fcbd8f07a18e8b7e17c6f17eac
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-24 12:42:15 +0300

    Add docstring examples

--- src/controller/time_controller.py
+++ src/controller/time_controller.py
@@ -1,5 +1,6 @@
+"""Module for the TimeController class."""
+
 from math import fmod
-from datetime import timedelta
 
 
 class TimeController:
@@ -14,6 +15,13 @@ class TimeController:
         hours, minutes, and seconds.
 
         See the class description for calculation behavior.
+
+        Examples
+        --------
+        >>> TimeController.convert_seconds(1)
+        (0, 0, 1)
+        >>> TimeController.convert_seconds(3728)
+        (1, 2, 8)
         """
         hours = int(seconds / 3600)
         minutes = int(fmod(seconds, 3600) / 60)
@@ -22,6 +30,23 @@ class TimeController:
 
     @staticmethod
     def seconds_to_string(seconds):
+        """Converts a number of seconds into a string representation
+        of hours, minutes, and seconds.
+
+        Parameters
+        ----------
+        seconds : int
+            The number of seconds to be converted.
+
+        Examples
+        --------
+        >>> TimeController.seconds_to_string(1)
+        '0m 1s'
+        >>> TimeController.seconds_to_string(3600)
+        '1h'
+        >>> TimeController.seconds_to_string(3728)
+        '1h 2m 8s'
+        """
         hours, minutes, seconds = TimeController.convert_seconds(seconds)
         negative_str = ''
         if hours < 0 or minutes < 0 or seconds < 0:
@@ -40,5 +65,22 @@ class TimeController:
 
     @staticmethod
     def timedelta_to_string(duration):
+        """Converts a timedelta object into a string representation
+        of hours, minutes, and seconds.
+
+        Parameters
+        ----------
+        duration : datetime.timedelta
+            The timedelta to be converted into a string.
+
+        Examples
+        --------
+        >>> TimeController.timedelta_to_string(timedelta(seconds=1))
+        '0m 1s'
+        >>> TimeController.timedelta_to_string(timedelta(seconds=3600))
+        '1h'
+        >>> TimeController.timedelta_to_string(timedelta(seconds=3728))
+        '1h 2m 8s'
+        """
         total_seconds = duration.total_seconds()
         return TimeController.seconds_to_string(total_seconds)
