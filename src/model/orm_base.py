00672209f78d00a2f6b5f5f41d51d9d3c3d78ab2
Sebastian Führ <s.fuehr1@gmail.com>
2023-08-04 13:08:36 +0300

    build(orm_base)!: change sqlalchemy base as DeclarativeBase class
    BREAKING CHANGE: define sqlalchemy Base as class instead of variable

--- src/model/orm_base.py
+++ src/model/orm_base.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    """Preferred over declarative_base() method to support typing.
+
+    Source:
+    https://docs.sqlalchemy.org/en/20/changelog/whatsnew_20.html#migrating-an-existing-mapping # noqa: E501
+    """
+
+    pass
 
 
 class DBConnection:
-    def __init__(self, db_path):
+    def __init__(self, db_path: str):
         self.db_engine = create_engine(f"sqlite://{db_path}", echo=False)
         self.Session = sessionmaker(bind=self.db_engine)
